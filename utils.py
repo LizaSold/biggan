6e4a90b47362c03998db7d3b0f96aa0b9125848e
LizaSold <elizabeth@soldatova.su>
2021-04-29 19:28:26 +0200

    update

--- utils.py
+++ utils.py
@@ -413,7 +413,7 @@ imsize_dict = {'I32': 32, 'I32_hdf5': 32,
                'C10': 32, 'C100': 32}
 root_dict = {'I32': 'ImageNet', 'I32_hdf5': 'ILSVRC32.hdf5',
              'I64': 'ImageNet', 'I64_hdf5': 'ILSVRC64.hdf5',
-             'I128': '0164_frog', 'I128_hdf5': 'ILSVRC128.hdf5',
+             'I128': 'frog', 'I128_hdf5': 'ILSVRC128.hdf5',
              'I256': 'ImageNet', 'I256_hdf5': 'ILSVRC256.hdf5',
              'C10': 'cifar', 'C100': 'cifar'}
 nclass_dict = {'I32': 1000, 'I32_hdf5': 1000,
