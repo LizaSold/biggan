eefefba917bc8000c7014f213dd684318a3e20d9
LizaSold <elizabeth@soldatova.su>
2021-05-04 15:58:26 +0200

    data utils added

--- utils.py
+++ utils.py
@@ -417,7 +417,7 @@ root_dict = {'I32': 'ImageNet', 'I32_hdf5': 'ILSVRC32.hdf5',
              'I128': 'ImageNet', 'I128_hdf5': 'ILSVRC128.hdf5',
              'I256': 'ImageNet', 'I256_hdf5': 'ILSVRC256.hdf5',
              'C10': 'cifar', 'C100': 'cifar', 
-             'ecoset': 'ECOSET', 'ecoset_hdf5': ILSVRCECO.hdf5}
+             'ecoset': 'ECOSET', 'ecoset_hdf5': 'ILSVRCECO.hdf5'}
 nclass_dict = {'I32': 1000, 'I32_hdf5': 1000,
                'I64': 1000, 'I64_hdf5': 1000,
                'I128': 1000, 'I128_hdf5': 1000,
